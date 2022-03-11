7d3f46a6373604617790bcbdbe985c3cd201c82b
Phuong Vy <phuongvy.maicogroup@gmail.com>
2022-03-11 15:59:35 +0700

    update getuploadlinkAsync

--- MaicoLand/Repositories/FileRepository.cs
+++ MaicoLand/Repositories/FileRepository.cs
@@ -38,25 +38,7 @@ namespace MaicoLand.Repositories
              config
            ) ;
         }
-        public Task<HttpResponseMessage> Upload(System.IO.FileInfo fileInfo)
-        {
-            
-                try
-                {
-                    var httpClient = new HttpClient();
-                    var multipartFormDataContent = new MultipartFormDataContent();
-                    httpClient.BaseAddress = new Uri(url);
-                    var fileContent = new ByteArrayContent(File.ReadAllBytes(fileInfo.FullName));
-                    multipartFormDataContent.Add(fileContent, "file", fileInfo.Name);
-                    return httpClient.PostAsync("upload", multipartFormDataContent);
-
-                }
-                catch
-                {
-                    return null;
-                }
-            
-        }
+ 
 
         public void DeleteFile(String filePath)
         {
@@ -64,24 +46,13 @@ namespace MaicoLand.Repositories
         }
         public  string GetUploadLinkAsync(string path , string contentType) 
         {
-            string extension = "";
-            switch (contentType)
-            {
-                case "image/png":
-                    extension = ".png";
-                    break;
-                 default:
-                    break; 
-            }
-            string fileName =path + "/"+ Guid.NewGuid().ToString() + extension;  
+          
             GetPreSignedUrlRequest request_generate_url = new GetPreSignedUrlRequest();
             request_generate_url.ContentType = contentType;
             request_generate_url.BucketName = bucketName;
-            request_generate_url.Key = fileName     ;
+            request_generate_url.Key = path     ;
             request_generate_url.Expires = DateTime.Now.AddMinutes(60);
             request_generate_url.Verb = HttpVerb.PUT;
-
-
             return client.GetPreSignedURL(request_generate_url);
 
 
@@ -97,78 +68,7 @@ namespace MaicoLand.Repositories
         }
         
         
-        public string SizeConverter(long bytes)
-        {
-            var fileSize = new decimal(bytes);
-            var kilobyte = new decimal(1024);
-            var megabyte = new decimal(1024 * 1024);
-            var gigabyte = new decimal(1024 * 1024 * 1024);
-
-            switch (fileSize)
-            {
-                case var _ when fileSize < kilobyte:
-                    return $"Less then 1KB";
-                case var _ when fileSize < megabyte:
-                    return $"{Math.Round(fileSize / kilobyte, 0, MidpointRounding.AwayFromZero):##,###.##}KB";
-                case var _ when fileSize < gigabyte:
-                    return $"{Math.Round(fileSize / megabyte, 2, MidpointRounding.AwayFromZero):##,###.##}MB";
-                case var _ when fileSize >= gigabyte:
-                    return $"{Math.Round(fileSize / gigabyte, 2, MidpointRounding.AwayFromZero):##,###.##}GB";
-                default:
-                    return "n/a";
-            }
-        }
-        public async Task<FileResponseResult>  GetFile(string path , string key) 
-        {
-            try
-            {
-
-                GetObjectRequest request_download = new GetObjectRequest();
-                request_download.BucketName = bucketName;
-                request_download.Key = path + key;
-                GetObjectResponse response = await client.GetObjectAsync(request_download);
-                //if (response.Headers.ContentType == "text/plain")
-                //{
-                using (Stream responseStream = response.ResponseStream)
-                {
-                    Console.WriteLine("Success");
-                    var bytes = ReadStream(responseStream);
-                    var download = new FileContentResult(bytes, response.Headers.ContentType);
-                    download.FileDownloadName = key;
-
-                    var result = new FileResponseResult(true, download);
-                    //Console.WriteLine(result.file.ContentType)
-                    Console.WriteLine(result.file.FileContents);
-                    return result;
-                }
-            }
-
-            //}
-
-            catch (Exception e)
-            {
-                return new FileResponseResult(false);
-            }
-            return new FileResponseResult(false);
-
-        }
-        public static byte[] ReadStream(Stream responseStream)
-        {
-            byte[] buffer = new byte[16 * 1024];
-            using (MemoryStream ms = new MemoryStream())
-            {
-                int read;
-                while ((read = responseStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ms.Write(buffer, 0, read);
-                }
-                return ms.ToArray();
-            }
-        }
-
-        public Task<HttpResponseMessage> Upload(Models.FileInfo uploadMeta)
-        {
-            throw new NotImplementedException();
-        }
+        
+       
     }
 }
--- MaicoLand/Repositories/InterfaceRepositories/IFileRepository.cs
+++ MaicoLand/Repositories/InterfaceRepositories/IFileRepository.cs
@@ -12,11 +12,8 @@ namespace MaicoLand.Repositories.InterfaceRepositories
 {
     public interface IFileRepository
     {
-        public Task<HttpResponseMessage> Upload(Models.FileInfo uploadMeta);
         public void DeleteFile(string filePath);
-        public string SizeConverter(long bytes);
         public string GetUploadLinkAsync(string path, string contentType);
-        public  Task<FileResponseResult> GetFile(string path, string key);
         public string GetLinkFile(string path);
     }
 }
